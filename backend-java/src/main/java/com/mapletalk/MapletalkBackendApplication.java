0e7e67df040c732faa564306f12c8fda641cd256
Harsh Raj <mail2harshraj2004@gmail.com>
2026-08-27 19:25:47 +0530

    feat: add Spring Boot backend foundation

---
+++ backend-java/src/main/java/com/mapletalk/MapletalkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mapletalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapletalkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapletalkBackendApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/mapletalk/MapletalkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapletalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapletalkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
